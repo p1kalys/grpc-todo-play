4107a6be29152dc733d4ab7bde713187cbb173d9
p1kalys <pavanemani14@gmail.com>
2024-02-27 15:41:35 +0530

    first commit

---
+++ client/index.js
@@ -0,0 +1,25 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const packageDefinition = protoLoader.loadSync('./todo.proto', {});
+
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+var todoService = protoDescriptor.TodoService;
+
+const client = new todoService('localhost:50051', grpc.credentials.createInsecure());
+
+client.listTodos({}, (err, todos) => {
+    if(!err) {
+        console.log(todos);
+        client.createTodo({id: 3, title: 'third todo', content: 'A new one'}, (err, todo) => {
+            if(!err) {
+                console.log('created a new todo');
+                client.listTodos({}, (err, todos) => {
+                    console.log("after insertion", todos);
+                })
+            } else {
+                console.log(err);
+            }
+        })
+    }
+});
